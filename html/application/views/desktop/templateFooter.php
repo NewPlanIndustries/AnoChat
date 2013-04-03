878fc08cb56f9684b8e373fa779f1993cdf2ea3a
Devin Rodriguez <devin@scaryuncledevin.com>
2013-04-03 13:52:11 -0700

    Updated links for upcoming SSL certificate changes

--- html/application/views/desktop/templateFooter.php
+++ html/application/views/desktop/templateFooter.php
@@ -9,7 +9,7 @@
 			<p>&copy; SnapChat.org, all rights reserved. SnapChat respects your privacy and no chats are ever stored. Chat service is provided with no guarantees.</p>
 		</div>
 		<div class="grid_2" align="right">
-			<a href="https://www.facebook.com/snapchat.org" title="Like us on Facebook" target="_blank"><img src="http://cdn1.iconfinder.com/data/icons/socialmediaicons_v120/16/facebook.png" alt="Like us on Facebook" title="Like us on Facebook"></a>
+			<a href="https://www.facebook.com/snapchat.org" title="Like us on Facebook" target="_blank"><img src="/assets/images/facebook.png" alt="Like us on Facebook" title="Like us on Facebook"></a>
 		</div>
 		<div class="clear"></div>
 	</div>
--- html/application/views/desktop/templateStyles.php
+++ html/application/views/desktop/templateStyles.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/assets/960.css<?=CACHE?>">
 <link rel="stylesheet" href="/assets/main.css<?=CACHE?>">
 <link rel="stylesheet" href="/assets/clientDesktop.css<?=CACHE?>">
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script src="/assets/functions.js<?=CACHE?>"></script>
